3465d6be500ffc7475a025cb68b45c6d52ffe6a9
Rafael <rafael.dev42@gmail.com>
2024-11-23 23:51:45 -0300

    Loops, starting with 'While'

--- src/While.java
+++ src/While.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class While {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int sum = 0;
+
+        while(x != 0) {
+            sum = sum + x;
+            x = sc.nextInt();
+        }
+        //The variable 'sum' accumulate the values entered by the user
+        //Then, the new number x is added again, this is 'repetitive structure' or simply a 'loop'
+        System.out.printf("Sum: %d", sum);
+
+        sc.close();
+    }
 }
