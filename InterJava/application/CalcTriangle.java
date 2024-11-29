ef3a5394a3990e497811c2cd6fefeb954c1ddef9
Rafael <rafael.dev42@gmail.com>
2024-11-29 19:47:26 -0300

    Rename

--- InterJava/application/CalcTriangle.java
+++ InterJava/application/CalcTriangle.java
@@ -5,7 +5,7 @@ import entities.Triangle;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class CalcTriangle {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
