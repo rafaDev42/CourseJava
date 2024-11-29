4425c5d54b448b3fc037935efdb0db1efa589bf7
Rafael <rafael.dev42@gmail.com>
2024-11-29 14:50:37 -0300

    Solving the triangle problem with OO

--- InterJava/application/Program.java
+++ InterJava/application/Program.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
@@ -25,21 +25,23 @@ public class Program {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-
+        double areaX = x.area();//Developing a method with the function 'Math'
+        double areaY = y.area();//turning more easily the implementation of this code
+                                //Making code more elegant and visually appealing. xD
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
 
-        if(areaX > areaY){
+        if (areaX > areaY) {
             System.out.println("Larger area: X");
-        }else {
+        } else {
             System.out.println("Larger area: Y");
         }
 
         sc.close();
     }
 }
+/*What are the benefits of calculating the area of Triangle a triangle using a METHOD within the Triangle CLASS?
+1) Code reuse: we eliminate the repeated code (calculating the areas of triangles x and y) in the main program.
+2) Delegation of responsibilities: the person responsible for knowing how to calculate the area of
+Triangle a triangle is the triangle itself. The logic for calculating the area should not be somewhere else.
+ */
