220a433dcd0237d2f304a91adf4296b57bd53216
Rafael <rafael.dev42@gmail.com>
2024-11-30 20:23:36 -0300

    Encapsulation, access modifiers, overloading, and constructors

--- InterJava/application/Store.java
+++ InterJava/application/Store.java
@@ -11,29 +11,28 @@ public class Store {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
-        System.out.print("Quantity in Stock: ");
-        product.quantity = sc.nextInt();
+        double price = sc.nextDouble();
+        //Constructor
+        Product product = new Product(name, price);
 
+        //accessing and changing data using getters and setters
+        product.setName("Computer");
+        System.out.println("Updated name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Updated price: " + product.getPrice());
 
         System.out.println("\nProduct Data: " + product);
-
         System.out.print("\nEnter the number of products to be added in stock: ");
         int quantity = sc.nextInt();
         product.addProducts(quantity);
-
         System.out.println("\nUpdated data: " + product);
-
         System.out.print("\nEnter the number of products to be removed from stock: ");
         quantity = sc.nextInt();
         product.removeProducts(quantity);
-
         System.out.println("\nUpdated data: " + product);
 
         sc.close();
